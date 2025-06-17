174693f82ddd3261fb378a62c235c0d781af5348
JohnImmanuel305 <johnimmanuel200535@gmail.com>
2025-06-17 21:13:41 +0530

    pls help

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Router, Route } from 'react-router-dom';
+import Home from './pages/home';
+import ExploreEvents from './pages/ExploreEvents';
+import About from './pages/About';
+import EventDetails from './pages/EventDetails';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Feedback from '/pages/Feedback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          baba ramji
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/explore" element= {<ExploreEvents />} />
+        <Route path="/about" element= {<About />} />
+        <Route path="/event/:id" element= {<EventDetails />} />
+        <Route path="/event/:id/chat" element= {<EventChat />} />
+        <Route path="/login" element= {<Login />} />
+        <Route path="/signup" element= {<Signup />} />
+        <Route path="/feedback" element= {<Feedback />} />
+      </Routes>
+    </Router>
+    
+
   );
 }
 
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Home() {
+    <h1>Home Page</h1>
+}
+export default Home;
