cc0f4b9658ea0ef2f3da2c5d119c8309296567be
SureshS03 <suthamani51@gmail.com>
2025-06-18 22:09:06 +0530

    done struct and added todo

---
+++ backend/internal/enity.go
@@ -0,0 +1,41 @@
+package internal
+
+import "time"
+
+type Users struct {
+	Id        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Bio       string    `json:"bio"`
+	Joined_at time.Time `json:"joined_at"`
+}
+
+type Events struct {
+	Id          int       `json:"event_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Place       string    `json:"place"`
+	Time        time.Time `json:"time"`
+	Managerid   Users     `json:"manager_id"`
+}
+
+type Event_Speaker struct {
+	Event_id Events `json:"event_id"`
+	User_id  Users  `json:"user_id"`
+}
+
+type RSVP struct {
+	User_id  Users     `json:"user_id"`
+	Event_id Events    `json:"event_id"`
+	Status   string    `json:"status"`
+	RSVPtime time.Time `json:"rsvp_time"`
+}
+
+type Comments struct {
+	Id         int       `json:"comment_id"`
+	User_id    Users     `json:"user_id"`
+	Event_id   Events    `json:"event_id"`
+	Msg        string    `json:"comment_msg"`
+	Created_at time.Time `json:"created_at"`
+}
