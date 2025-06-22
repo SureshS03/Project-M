16abb688ed8587ad76bef18c1ae182fdacef7312
Suriya1306 <suriyarajapandian@gmail.com>
2025-06-22 23:03:51 +0530

    not working postmancss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require('@tailwindcss/postcss'),
+    require('postcss-flexbugs-fixes'),
+    require('tailwindcss'),
     require('autoprefixer'),
   ],
-};
+}
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ResponsiveNavbar from '../components/responsivenavbar';
 import Hero from '../components/hero';
 import FeatureSection from '../components/featuresection';
-import AnimatedClock from '../components/animatedclock';
 import RankBoard from '../components/rankboard';
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       <ResponsiveNavbar />
       <Hero />
       <FeatureSection />
-      <AnimatedClock />
       <RankBoard />
     </>
   );
