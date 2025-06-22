74af719c1a140ae1cb27f0eb81d5c4e5f0c90cfc
Bharathwaaj18 <mtgbharathwaaj@gmail.com>
2025-06-22 23:40:41 +0530

    Add custom user model

--- backend/project_backend/main/models.py
+++ backend/project_backend/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        error_messages={
+            'unique': "A user with that username already exists."
+        } 
+    )
+    password = models.CharField(
+        max_length=128,
+        help_text="Required. 128 characters or fewer. Letters, digits and @/./+/-/_ only."
+    )    
+    email = models.EmailField(
+        unique=True,
+        error_messages={
+            'unique': "A user with that email already exists."
+        },      
+        help_text="Required. Enter a valid email address."
+    )   
+
